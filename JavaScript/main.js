1fd26d5a9d8019037f7234299f5d134b24d4db98
hawhdgus <hawhdgus@naver.com>
2020-07-03 12:35:51 +0900

    Finish_bread

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,6 +1,6 @@
 /* 오른쪽,왼쪽 이동 */
 (function() {
-
+    KeyboardEnabled = true
     const eventClick = document.querySelector('.event-click');
     const rightWall = document.querySelector('.right-wall');
     let i = 1;
@@ -14,6 +14,8 @@
         nowContainer.classList.add('hidden-left');
         nextContainer.classList.remove('hidden-right');
         nextContainer.classList.add('move-on');
+        console.log(event);
+        console.log(event.target);
       };
     };
   
@@ -26,10 +28,39 @@
         nowContainer.classList.add('hidden-right');
         nextContainer.classList.remove('hidden-left');
         nextContainer.classList.add('move-on');
+        console.log(event);
+        console.log(event.target);
       };
     };
   
     eventClick.addEventListener('click', goRight);
     eventClick.addEventListener('click', goLeft);
-  
+    
+    window.addEventListener('keydown', function( ev ) {
+      if (i !== 1 && ev.keyCode === 37) {
+        const nowContainer = document.querySelector(`.container:nth-child(${i})`);
+        i--;
+        const nextContainer = document.querySelector(`.container:nth-child(${i})`);
+        nowContainer.classList.remove('move-on');
+        nowContainer.classList.add('hidden-right');
+        nextContainer.classList.remove('hidden-left');
+        nextContainer.classList.add('move-on');
+        console.log(event);
+        console.log(event.target);
+      }
+    });
+
+    window.addEventListener('keydown', function( ev ) {
+      if (i !== 4 && ev.keyCode === 39) {
+        const nowContainer = document.querySelector(`.container:nth-child(${i})`);
+        i++;
+        const nextContainer = document.querySelector(`.container:nth-child(${i})`);
+        nowContainer.classList.remove('move-on');
+        nowContainer.classList.add('hidden-left');
+        nextContainer.classList.remove('hidden-right');
+        nextContainer.classList.add('move-on');
+        console.log(event);
+        console.log(event.target);
+      }
+		});
   })();
